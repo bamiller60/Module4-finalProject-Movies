491ebac1fdbf50e51451e56f9f0fe7eec6051eaf
Brad <bamiller6024@gmail.com>
2025-04-12 14:28:56 -0400

    Second Commit Change button icon onclick

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function openMenu() {
  function closeMenu() {
  document.body.classList.remove('menu--open');
  }
+
+ function changeIcon() {
+    const iconWrapper = document.querySelector(".btn");
+
+    iconWrapper.classList += ' '
+    if (icon.classList.contains("fa-magnifying-glass")) {
+        icon.classList.remove("fa-magnifying-glass");
+        icon.classList.add("fa-spinner");
+    } 
+    //else {
+    //    icon.classList.remove("fa-heart");
+    //    icon.classList.add("fa-star");
+    //}
+}
