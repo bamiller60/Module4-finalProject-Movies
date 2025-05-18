42143d93208e96450771b2a5b6de9134ccbe02cf
Brad <bamiller6024@gmail.com>
2025-05-18 16:47:20 -0400

    Finished movies.js/loading state

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const iconWrapper = document.querySelector(".btn");
+const searchData = document.querySelector(".search__container");
+
 function openMenu() {
     document.body.classList.add('menu--open'); 
  }
@@ -6,13 +9,29 @@ function openMenu() {
  document.body.classList.remove('menu--open');
  }
 
- function changeIcon() {
-     const iconWrapper = document.querySelector(".btn");
-     
-     
-     iconWrapper.addEventListener('click', () => {
-         iconWrapper.classList.add('btn__loading');
+ function changeIcon() {    
+     iconWrapper.addEventListener('click', () => {//Why does it take two button presses to work corectly inside these braces?//
         })
         iconWrapper.innerHTML = `<i class="fa-solid fa-spinner"></i>`;
+        iconWrapper.classList.add('btn__loading');
     }
 
+
+
+//async function main(event) {
+//    const url = `https://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2&s=${encodeURI(event)}`;
+//    const search = await fetch(`${url}`);
+//    const data = await search.json();
+//    localStorage.setItem('movieData', JSON.stringify(data.Search));
+//    window.location.href = `${window.location.origin}/movies.html`;
+//}
+//
+//
+//searchData.addEventListener('submit', (event) => {
+//    const searchValue = document.getElementById("searchbar").value;
+//
+//   event.preventDefault();
+//   // localStorage.setItem('key', searchTerm);
+//   // const searchKey = localStorage.getItem('key');
+//    main(searchTerm); 
+//})
--- movies.js
+++ movies.js
@@ -1,50 +1,80 @@
 // DATA REQUESTS
-// http://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2
+// http://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2TS
 // POSTER REQUESTS
 // http://img.omdbapi.com/?apikey=8efecdd2&
 
-//async function main() {
-//    const movies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2&s=fast");
-//    const movieData = await movies.json();
-//    const movieListEl = document.querySelector('.user-card')
-//
-//    movieListEl.innerHTML = 
-//    movieData.Search.map(
-//    (movie) => 
-//    `<div class="user-card">
-//        <div class="user-card__container">
-//            <h3>Title</h4>
-//            <p><b>Poster:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-//            <p><b>Year:</b> XXXX</p>
-//            <p><b>Type:</b> Movie</p>
-//        </div>
-//    </div>`
-//)  
-//.join("")
-//}
-//
-//main();
-async function main() {
-    const movies = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2&s=fast");
-    const movieData = await movies.json();
-    const movieListEl = document.querySelector('.user-card');
+ const moviesBody = document.querySelector('.movies');
+ const moviesWrapper = document.querySelector('.movie-list');
+ const inputEl = document.querySelector('.movie__name');
+ const searchTerm = document.querySelector('.search__term')
+ 
 
-    movieListEl.innerHTML = movieData.Search.map(
-        (movies) =>
-        `<div class="user-card">
-        <div class="user-card__container">
-            <h3>${movies.Title}</h3>
-            <p><b>Poster:</b><a>${movies.Poster}</a></p>
-            <p><b>Year:</b> ${movies.Year}</p>
-            <p><b>Type:</b> ${movies.Type}</p>
-        </div>
-    </div>`
-        ).join()
+let currentArr = [];
+
+function searchMovies(event) {
+    getMovies(event.target.value);
+    inputEl.innerHTML = event.target.value;
+    searchTerm.innerHTML = event.target.value;
+    moviesBody.classList += (' movies__loading');
+ }
+
+function load(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getMovies(movieSearch) {      
+    const movies = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=8efecdd2&s=${movieSearch}`);
+    const moviesData = await movies.json();
+    await load(1500);    
+    currentArr = moviesData.Search;    
+    displayMovies(currentArr);
     }
 
-main();
-
-function showMovieLists(id) {
-   
+function displayMovies(currentArr) {
+    moviesWrapper.innerHTML = currentArr
+    .map((movie) => {
+    return `<div class="movie-card" onclick="showMovie('${movie.Title}')">
+                <div class="movie-card__container">
+                    <img src="${movie.Poster}" alt="${movie.Title}">
+                    <h3>${movie.Title}</h3>
+                    <p><b>Year:</b> ${movie.Year}</p>
+                    <p><b>Type:</b> ${movie.Type}</p>
+                    <p><b>IMDB ID:</b> ${movie.imdbID}</p>
+                </div>
+            </div>`;         
+    })
+    .slice(0, 6).join("");    
 }
+
+function filterMovies(event) {
+   const filterValue = event.target.value;
+
+   let filteredMovies = [...currentArr];
+
+    if (filterValue === "newest") {
+         filteredMovies.sort((a, b) => b.Year - a.Year);
+    } else if (filterValue === "oldest") {
+         filteredMovies.sort((a, b) => a.Year - b.Year);
+    }
+    
+    displayMovies(filteredMovies);
+}
+
+
+
+
+
+
+
+
+            
+            
+ 
+
+
+
+
+
+
+
 
